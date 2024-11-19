9fa449f3bbaabd5e0eebe40727dda3ab6564703e
Suhas Unni <suhasunni6@gmail.com>
2024-11-19 17:04:18 -0500

    fix bugs and add comments

--- main.c
+++ main.c
@@ -3,34 +3,44 @@ int main()
 {
     while (1)
     {
-        int digit;
-        if (digit != 2)
+        int digit = 0;
+        if (digit != 2) // pin B8
         {
             HAL_GPIO_WritePin(GPIOB, GPIO_PIN_8, GPIO_PIN_SET);
         }
-        if (digit != 5 && digit != 6)
+        if (digit != 5 && digit != 6) // pin A6
         {
             HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, GPIO_PIN_SET);
         }
-        if (digit != 1 && digit != 7 && digit != 0)
+        if (digit != 1 && digit != 7 && digit != 0) // pin C7
         {
             HAL_GPIO_WritePin(GPIOC, GPIO_PIN_7, GPIO_PIN_SET);
         }
-        if (digit != 1 && digit != 4)
+        if (digit != 1 && digit != 4) // pin A7
         {
             HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, GPIO_PIN_SET);
         }
-        if (digit != 1 && digit != 7 && digit != 4 && digit != 9)
+        if (digit != 1 && digit != 7 && digit != 4 && digit != 9) // pin B9
         {
             HAL_GPIO_WritePin(GPIOB, GPIO_PIN_9, GPIO_PIN_SET);
         }
-        if (digit == 1 || digit == 6 || digit == 8)
+        if (digit == 2 || digit == 6 || digit == 8 || digit == 0) // pin A5
         {
             HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET);
         }
-        if (digit == 4 || digit == 6 || digit == 8 || digit == 5 || digit == 9)
+        if (digit == 4 || digit == 6 || digit == 8 || digit == 5 || digit == 9 || digit == 0) // pin B6
         {
-            HAL_GPIO_WritePin(GPIOB, GPIO_PIN_7, GPIO_PIN_SET);
+            HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, GPIO_PIN_SET);
         }
+        HAL_Delay(5000);
+        // reset pins after 5 seconds
+        HAL_GPIO_WritePin(GPIOB, GPIO_PIN_8, GPIO_PIN_RESET);
+        HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET);
+        HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, GPIO_PIN_RESET);
+        HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, GPIO_PIN_RESET);
+        HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, GPIO_PIN_RESET);
+        HAL_GPIO_WritePin(GPIOB, GPIO_PIN_9, GPIO_PIN_RESET);
+        HAL_GPIO_WritePin(GPIOC, GPIO_PIN_7, GPIO_PIN_RESET);
+        break;
     }
 }
